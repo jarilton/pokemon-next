fe490f6c4e27f525e3d504e1635cae7199bed3f7
jarilton <jariltonpereira02@hotmail.com>
2022-12-19 23:15:45 -0300

    Ajustes rodapé

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -5,11 +5,17 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
   background-color: #333;
+  color: #fff;
   height: 10rem;
-  margin-top: 2rem;
+  margin-top: 3rem;
   border-top: 3px solid #111;;
 `;
 
-export const Info = styled.div``;
+export const Info = styled.div`
+    display: flex;
+    gap: 0.5rem;
+`;
 
-export const Destak = styled.div``;
+export const Destak = styled.div`
+    font-weight: bold;
+`;
